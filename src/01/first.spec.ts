d115f43962c4c28e661b1f61352dd025257ac445
Evan Sutherland <esutherland@pollyex.com>
2023-12-02 08:32:07 -0600

    1.1

--- src/01/first.spec.ts
+++ src/01/first.spec.ts
@@ -1,8 +1,19 @@
-import { expect, test } from 'vitest'
-import { solve } from '@/01/first'
+import { expect, expectTypeOf, test } from 'vitest'
+import { Solution, solution } from '@/01/first'
+import { Example1, example1, input } from '@/01/input'
 
-test('always returns 123', () => {
-  const response = solve()
+test('given test input, always returns 142', () => {
+  type Response = Solution<Example1>
+  const input = example1.split('\n')
 
-  expect(response).toBe('123')
+  const response = solution(input)
+
+  expectTypeOf<Response>().toMatchTypeOf<142>()
+  expect(response).toBe(142)
+})
+
+test('given actual input, always returns correct answer', () => {
+  const response = solution(input.split('\n'))
+
+  expect(response).toBe(55971)
 })
--- src/01/first.ts
+++ src/01/first.ts
@@ -1,5 +1,43 @@
-import { input } from '@/01/input'
+export type Count<T extends { length: number }> = T['length'] & number
 
-export function solve(): string {
-  return input
+export type ToArray<TLength extends number, TValues extends unknown[] = []> = TValues extends { length: TLength } ? TValues : ToArray<TLength, [null, ...TValues]>
+
+export type Sum<A extends number, B extends number> = Count<[...ToArray<A>, ...ToArray<B>]>
+
+export type Combine<A extends number, B extends number> = ToNumber<`${A}${B}`>
+
+export type ToNumber<T extends string> = T extends `${infer NumberValue extends number}`
+  ? NumberValue
+  : never
+
+export type Split<T extends string> = T extends `${infer First}${infer Rest}` ? [First, ...Split<Rest>] : []
+
+export type NumbersOnly<T extends unknown[], TReturn extends number[] = []> = T extends [infer First extends string, ...infer Rest]
+  ? ToNumber<First> extends never
+    ? NumbersOnly<Rest, TReturn>
+    : NumbersOnly<Rest, [...TReturn, ToNumber<First>]>
+  : TReturn
+
+export type First<TArray extends number[]> = TArray extends [infer First extends number, ...number[]]
+  ? First
+  : 0
+
+export type Last<TArray extends number[]> = TArray extends [...number[], infer Last extends number]
+  ? Last
+  : 0
+
+export type Decode<T extends string, TArray extends number[] = NumbersOnly<Split<T>>> = Combine<First<TArray>, Last<TArray>>
+
+export type Solution<T extends Readonly<string[]>, TSum extends number = 0> = T extends [infer Code extends string, ...infer Rest extends string[]]
+  ? Solution<Rest, Sum<TSum, Decode<Code>>>
+  : TSum
+
+export function solution(input: string[]): number {
+  return input.reduce((sum, code) => {
+    const [first, ...rest] = code.match(/\d/g) ?? []
+
+    const value = Number(`${first}${rest.pop() ?? first}`)
+
+    return sum + value
+  }, 0)
 }
